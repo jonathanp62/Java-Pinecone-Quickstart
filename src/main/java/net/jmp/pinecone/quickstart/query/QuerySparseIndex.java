583a1031b5bae068af97907a5df519800df1a49c
Jonathan Parker <jonathanp62@gmail.com>
2025-06-11 08:15:44 -0400

    WIP commit

--- src/main/java/net/jmp/pinecone/quickstart/query/QuerySparseIndex.java
+++ src/main/java/net/jmp/pinecone/quickstart/query/QuerySparseIndex.java
@@ -30,11 +30,25 @@ package net.jmp.pinecone.quickstart.query;
 
 import com.mongodb.client.MongoClient;
 
+import io.pinecone.clients.Index;
+import io.pinecone.clients.Inference;
 import io.pinecone.clients.Pinecone;
+
+import io.pinecone.unsigned_indices_model.QueryResponseWithUnsignedIndices;
+import io.pinecone.unsigned_indices_model.ScoredVectorWithUnsignedIndices;
+
+import static java.lang.Integer.toUnsignedLong;
+
+import java.util.*;
 
 import net.jmp.pinecone.quickstart.Operation;
 
 import static net.jmp.util.logging.LoggerUtils.*;
+
+import org.openapitools.inference.client.ApiException;
+
+import org.openapitools.inference.client.model.Embedding;
+import org.openapitools.inference.client.model.EmbeddingsList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -79,7 +93,69 @@ public final class QuerySparseIndex extends Operation {
             this.logger.trace(entry());
         }
 
-        this.logger.info("Hello from the query sparse index operator");
+        if (this.doesSparseIndexExist() && this.isSparseIndexLoaded()) {
+            final Map<String, Object> parameters = new HashMap<>();
+            final Inference client = this.pinecone.getInferenceClient();
+
+            List<Float> sparseValues = new ArrayList<>();
+            List<Integer> sparseIndices = new ArrayList<>();
+
+            parameters.put("input_type", "query");
+            parameters.put("truncate", "END");
+
+            EmbeddingsList sparseEmbeddings = null;
+
+            try {
+                sparseEmbeddings = client.embed(this.sparseEmbeddingModel, parameters, List.of(this.queryText));
+            } catch (ApiException e) {
+                this.logger.error(e.getMessage());
+            }
+
+            if (sparseEmbeddings != null) {
+                final List<Embedding> embeddingsList = sparseEmbeddings.getData();
+
+                assert embeddingsList.size() == 1;
+
+                sparseValues = embeddingsList.getFirst().getSparseEmbedding().getSparseValues();
+                sparseIndices = embeddingsList.getFirst().getSparseEmbedding().getSparseIndices();
+
+                if (this.logger.isDebugEnabled()) {
+                    this.logger.debug("Query sparse embeddings: {}: {}", this.queryText, sparseEmbeddings.toJson());
+                }
+            }
+
+            if (!sparseValues.isEmpty() && !sparseIndices.isEmpty()) {
+                final List<Long> sparseIndicesAsLongs = new ArrayList<>(sparseIndices.size());
+
+                for (Integer sparseIndex : sparseIndices) {
+                    sparseIndicesAsLongs.add(toUnsignedLong(sparseIndex));
+                }
+
+                try (final Index index = this.pinecone.getIndexConnection(this.sparseIndexName)) {
+                    final QueryResponseWithUnsignedIndices response = index.query(
+                            10,
+                            Collections.emptyList(),
+                            sparseIndicesAsLongs,
+                            sparseValues,
+                            null,
+                            this.namespace,
+                            null,
+                            true,
+                            true
+                    );
+
+                    final List<ScoredVectorWithUnsignedIndices> matches = response.getMatchesList();
+
+                    if (this.logger.isDebugEnabled()) {
+                        this.logger.debug("Matches: {}", matches);
+                    }
+                }
+            } else {
+                this.logger.error("The sparse embeddings are empty");
+            }
+        } else {
+            this.logger.info("Sparse index does not exist or is not loaded: {}", this.sparseIndexName);
+        }
 
         if (this.logger.isTraceEnabled()) {
             this.logger.trace(exit());
